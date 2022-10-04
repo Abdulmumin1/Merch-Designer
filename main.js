d638efda7f592605eb55ac16d06b9575a5e111f4
Abdulmumin Abdulkarim <avdorr12345@gmail.com>
2022-10-04 11:25:34 +0100

    moved to vite

---
+++ main.js
@@ -0,0 +1,37 @@
+import "./dist/style.css";
+import {
+  updateLabelColor,
+  updateShirtColor,
+  changeLabelFont,
+  changeLabelText,
+  changeRightOffset,
+  changeTopOffset,
+} from "./src/app";
+import javascriptLogo from "./javascript.svg";
+import { setupCounter } from "./counter.js";
+
+const select = (e) => {
+  return document.querySelector(e);
+};
+
+const startupEvents = () => {
+  let shirt = select("#shirt");
+  let label = select("#label");
+  let labelInput = select("#label-text-input");
+  let labelContainer = select("#label-container");
+  let labelColorInp = select("#label-color");
+  let shirtColorInp = select("#shirt-color");
+
+  let fontSlider = select("#font-slider");
+  let topSlider = select("#top-slider");
+  let rightSlider = select("#right-slider");
+  changeLabelText(labelInput, label);
+  updateLabelColor(labelColorInp, label);
+  updateShirtColor(shirtColorInp, shirt);
+  changeLabelFont(fontSlider, label);
+
+  changeTopOffset(topSlider, labelContainer);
+  changeRightOffset(rightSlider, labelContainer);
+};
+
+window.addEventListener("load", startupEvents, false);
---
+++ src/app.js
@@ -0,0 +1,120 @@
+// const startup = () => {
+//   let colourInp = document.querySelector("#label-color");
+//   colourInp.addEventListener("input", updateLabelColor, false);
+
+//   let colorSht = document.querySelector("#shirt-color");
+//   colorSht.addEventListener("input", updateShirtColor, false);
+
+//   let inp = document.querySelector("#label-text");
+//   inp.addEventListener("input", changeLabelText, false);
+
+//   let fontSlider = document.querySelector("#font-slider");
+//   fontSlider.addEventListener("input", changeLabelFont, false);
+//   let topSlider = document.querySelector("#top-slider");
+//   topSlider.addEventListener("input", changeTopOffset, false);
+//   let rightSlider = document.querySelector("#right-slider");
+//   rightSlider.addEventListener("input", changeRightOffset, false);
+//   let css = document.querySelector("#css");
+//   css.addEventListener("input", loadCss, false);
+
+//   changeLabelText(inp);
+//   updateLabelColor(colourInp);
+//   updateShirtColor(colorSht);
+//   changeLabelFont(fontSlider);
+//   changeTopOffset(topSlider);
+//   changeRightOffset(rightSlider);
+//   loadCss(css);
+//   // setCenter();
+//   //   inp.addEventListener("propertychange", changeLabelText, false);
+// };
+
+export const updateLabelColor = (triggerer, reactor) => {
+  const changecolor = (e) => {
+    if (e.target != undefined) {
+      reactor.style.color = e.target.value;
+    } else {
+      reactor.style.color = reactor.value;
+    }
+  };
+  triggerer.addEventListener("input", changecolor, false);
+  changecolor(reactor);
+};
+
+export const updateShirtColor = (triggerer, reactor) => {
+  const shirt = reactor;
+  const changecolor = (e) => {
+    shirt.style.fill = e.target.value;
+  };
+  triggerer.addEventListener("input", changecolor, false);
+};
+
+export const changeLabelFont = (triggerer, reactor) => {
+  let label = reactor;
+  const updatefont = (e) => {
+    if (e.target != undefined) {
+      label.style.fontSize = `${e.target.value}px`;
+    } else {
+      label.style.fontSize = `${e.value}px`;
+    }
+  };
+  triggerer.addEventListener("input", updatefont, false);
+  updatefont(reactor);
+};
+
+export const changeTopOffset = (triggerer, reactor) => {
+  let label = reactor;
+  const updatetop = (e) => {
+    if (e.target != undefined) {
+      label.style.top = `${e.target.value}%`;
+    } else {
+      label.style.top = `${e.value}%`;
+    }
+  };
+  triggerer.addEventListener("input", updatetop, false);
+  updatetop(reactor);
+};
+
+export const changeRightOffset = (triggerer, reactor) => {
+  let label = reactor;
+  const updateright = (e) => {
+    if (e.target != undefined) {
+      label.style.right = `${100 - e.target.value}%`;
+    } else {
+      label.style.right = `${100 - e.value}%`;
+    }
+  };
+  triggerer.addEventListener("input", updateright, false);
+  updateright(reactor);
+};
+export const changeLabelText = (triggerer, reactor) => {
+  let label = reactor;
+
+  const updatetext = (e) => {
+    label.innerText = e.target.value;
+  };
+  triggerer.addEventListener("input", updatetext, false);
+};
+const loadCss = (e, element) => {
+  let style = document.querySelector(element);
+  console.log(style.innerHTML);
+  if (e.value != undefined) {
+    style.innerHTML = e.value;
+  } else {
+    style.innerHTML = e.target.value;
+  }
+};
+
+const setCenter = () => {
+  let label = document.querySelector(element);
+  label.style.top = `calc(50% - ${label.style.length})px`;
+};
+
+const getImage = (id) => {
+  domtoimage.toPng(document.getElementById(id)).then((dataUrl) => {
+    let image = new Image();
+    image.src = dataUrl;
+    document.body.appendChild(image);
+  });
+};
+
+// window.addEventListener("load", startup, false);
