ba588338fe64e203c030aa28d7296467978194aa
Abdulmumin Abdulkarim <avdorr12345@gmail.com>
2022-10-11 15:17:19 +0100

    feat

--- src/components.js
+++ src/components.js
@@ -1,7 +1,7 @@
 export const iconControls = () => {
   return `
   <div class='flex flex-col hidden' id="iconControls">
-    <p>Icon</p>
+    <p class='mb-1 bg-gray-700 text-gray-900 rounded-xl p-2''>Icon</p>
     <label>
         size:
         <input
@@ -40,7 +40,7 @@ export const iconControls = () => {
 export const labelControls = () => {
   return `
   <div class='flex flex-col ' id="labelControls">
-  <p>Text</p>
+  <p class='mb-1 bg-gray-700 text-gray-900 rounded-xl p-2'>Text</p>
   <label>
       Font:
       <input
@@ -73,6 +73,13 @@ export const labelControls = () => {
         />
       </label>
     
+      </div>
+      <p class='mb-2 bg-gray-700 text-gray-900 rounded-xl p-2''>Align</p>
+      <div class='text-gray-900'>
+      <input type='button' value='left' class='bg-gray-400 p-3 rounded-xl'/>
+      <input type='button' value='center'  class='bg-gray-400 p-3 rounded-xl'/>
+      <input type='button' value='right'  class='bg-gray-400 p-3 rounded-xl'/>
+
       </div>
     </div>`;
 };
